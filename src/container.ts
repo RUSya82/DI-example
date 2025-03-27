c738bc1c8d7f86bff72904f7c1f61137a97dd8fc
RuslanSalimov <mail-spam2012@yandex.ru>
2025-03-28 01:09:37 +0500

    feat: step-5 fdd comments

--- src/container.ts
+++ src/container.ts
@@ -6,13 +6,18 @@ export class Container {
     // }
 
     resolve(provider: any){
+        // Если нет метадаты Injectable - ошибка
         if(!Reflect.getMetadata('injectable', provider)){
             throw new Error(`Class ${provider.name} is not marked as @Injectable`)
         }
+        // Извлекаем зависимости, если их нет - пустой массив
         const params = Reflect.getMetadata("design:paramtypes", provider) || [];
+        // Перебираем все зависимости, и рекурсивно их разрешаем
         const dependencies = params.map((param: any) => this.resolve(param))
 
+        // создаем инстанс, передавая его зависимости
         const instance = new provider(...dependencies);
+        // Добавляем в Map  с провайдерами
         this.providers.set(provider.name, instance)
         return instance;
     }
