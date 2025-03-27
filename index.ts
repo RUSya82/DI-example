f46ae7b56bdef5d56fc7aa8858954a81f26461d3
RuslanSalimov <mail-spam2012@yandex.ru>
2025-03-27 16:18:38 +0500

    Prepare project

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
-import {Container} from "./src/container";
-import {Controller} from "./src/controller";
-
-const container = new Container();
-const controller = container.resolve(Controller);
-controller.start();
+console.log('I`m here');
